5cb5b50fb467d2042520e1abb6e20c1587d89860
noizhardware <noisefuel@gmail.com>
2020-08-08 16:38:22 +0200

    lu

--- baofiles.c
+++ baofiles.c
@@ -7,13 +7,13 @@ int main(){
      int loopTable = 0;
      char** lines;
      lines = fileToLines("test.txt");
-     
+
      printf("==fileToLines:\n");
-     
+
      while(lines[loopTable][0]!='\0'){
      printf("%s\n", lines[loopTable]);
      loopTable++;}
-     
+
      freeStringTable(lines);
-     
+
      return 0;}
