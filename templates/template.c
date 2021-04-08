780aa3779353eec79e13ebb2a434ad06cff1fdb5
noizhardware <noisefuel@gmail.com>
2021-04-08 15:11:32 +0200

    nj

---
+++ templates/template.c
@@ -0,0 +1,35 @@
+/*** TODO
+
+*/
+
+#define FILENAME_VERSION "2021c06-1139"
+
+/*** DEFINES */
+/* DEFINES end. */
+
+/*** INCLUDES */
+/* INCLUDES end. */
+
+/*** TYPEDEFS */
+/* TYPEDEFS end. */
+
+/*** GLOBALS */
+/* GLOBALS end. */
+
+/*** FUNCTION DECLARATIONS */
+/* FUNCTION DECLARATIONS end. */
+
+
+
+
+/*** MAIN */
+int main(int argc, char* argv[]){
+     
+     (void)argc;
+     (void)argv;
+     return 0;
+}
+/* MAIN end. */
+
+/*** FUNCTION DEFINITIONS */
+/* FUNCTION DEFINITIONS end. */
---
+++ templates/template.h
@@ -0,0 +1,35 @@
+#ifndef _FILENAME_H_
+#define _FILENAME_H_
+
+#define FILENAME_VERSION "2021c06-1139"
+
+/*** TODO
+
+*/
+
+/*** INCLUDES */
+/* INCLUDES end. */
+
+#ifdef __cplusplus
+     extern "C" {
+#endif
+
+/*** DEFINES */
+/* DEFINES end. */
+
+/*** TYPEDEFS */
+/* TYPEDEFS end. */
+
+/*** GLOBALS */
+/* GLOBALS end. */
+
+/*** FUNCTION DECLARATIONS */
+/* FUNCTION DECLARATIONS end. */
+
+/*** FUNCTION DEFINITIONS */
+/* FUNCTION DEFINITIONS end. */
+
+#ifdef __cplusplus
+     }
+#endif
+#endif /* _FILENAME_H_ */
