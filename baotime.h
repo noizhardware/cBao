a1bbe0715acb2f95c83340fc0b3a71d44ccdbf72
noizhardware <noisefuel@gmail.com>
2021-04-16 20:37:25 +0200

    nj

--- baotime.h
+++ baotime.h
@@ -1,52 +1,123 @@
 #ifndef _BAOTIME_H_
 #define _BAOTIME_H_
 
-/* 2020w20-1821 */
+#define BAOTIME_VERSION "2021d15-1635"
 
-/*
- * ANSI C compliant
+/*** TODO
  * need to use -Wno-long-long >> define BAOTIME_LONGLONG_ENABLED if you do so
- */
+*/
 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-     #include <time.h>
-     #include <windows.h>
+
+/*** INCLUDES */
+#ifndef _POSIX_C_SOURCE
+     #define _POSIX_C_SOURCE 199309L
 #endif
 
-#ifdef __linux__
-     #include <sys/time.h>
+     #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+          #include <time.h>
+          #include <windows.h>
+     #endif
+
+     #ifdef __linux__
+          #include <sys/time.h>
+     #endif
+
+     #ifdef WIN32
+     #elif _POSIX_C_SOURCE >= 199309L
+     #include <time.h> /* for nanosleep */
+     #else
+     #include <unistd.h> /* for usleep */
+     #endif /* OS switch */
+     /**********************************/
+
+     #include <stdbool.h>
+     #include <math.h>
+     #include "baostring.h"
+
+     /* gettimeofday for WINDOWS */
+     #ifdef WIN32
+     #include <stdint.h>
+     /* already defined in c:\mingw\include\sys\time.h:55:8 */
+     /*typedef struct timeval{*/
+     /*     time_t     tv_sec;*/    /* seconds */
+     /*     useconds_t tv_usec;*/   /* microseconds */
+     /*} timeval;*/
+
+     /* already defined in c:\mingw\include\time.h:382:22 */
+     /*typedef struct timezone{*/
+          /*int tz_minuteswest; */    /* minutes west of Greenwich */
+          /*int tz_dsttime;  */       /* type of DST correction */
+     /*} timezone;*/
+     
+/* INCLUDES end. */
+
+#ifdef __cplusplus
+     extern "C" {
 #endif
 
-#ifdef WIN32
-#elif _POSIX_C_SOURCE >= 199309L
-#include <time.h> /* for nanosleep */
-#else
-#include <unistd.h> /* for usleep */
-#endif /* OS switch */
-/**********************************/
+/*** DEFINES */
+/* DEFINES end. */
 
-#include <stdbool.h>
-#include <math.h>
-#include "baostring.h"
+/*** TYPEDEFS */
+/* TYPEDEFS end. */
 
-/* gettimeofday for WINDOWS */
-#ifdef WIN32
-#include <stdint.h>
-/* already defined in c:\mingw\include\sys\time.h:55:8 */
-/*typedef struct timeval{*/
-/*     time_t     tv_sec;*/    /* seconds */
-/*     useconds_t tv_usec;*/   /* microseconds */
-/*} timeval;*/
+/*** GLOBALS */
+struct timeval __BAOTIME_H__stop, __BAOTIME_H__startTime, __BAOTIME_H__stopTime, __BAOTIME_H__difference, __BAOTIME_H__savedTime, __BAOTIME_H__updatedTime, __BAOTIME_H__splitTime;
+bool __BAOTIME_H__running = false;
 
-/* already defined in c:\mingw\include\time.h:382:22 */
-/*typedef struct timezone{*/
-     /*int tz_minuteswest; */    /* minutes west of Greenwich */
-     /*int tz_dsttime;  */       /* type of DST correction */
-/*} timezone;*/
+     struct timespec baotime_start, baotime_end;
+     double baotime_time_spent;
+/* GLOBALS end. */
+
+/*** FUNCTION DECLARATIONS */
+     static __inline__ float sec_to_hm(unsigned long int sec);
+     static __inline__ unsigned long int hm_to_sec(float hm);
+
+     #ifdef BAOTIME_LONGLONG_ENABLED
+          static __inline__ void pauseTimer();
+          static __inline__ void startTimer();
+          static __inline__ void splitTimer();
+          static __inline__ void getTime();
+          static __inline__ void stopTimer();
+          static __inline__ void timertoggle();
+     #endif /* #ifdef BAOTIME_LONGLONG_ENABLED */
+     
+     #ifndef BAOTIME_LONGLONG_ENABLED
+          static __inline__ void startTimer();
+          static __inline__ double getTimerNs();
+          static __inline__ double getTimerMs();
+     #endif
+
+     static __inline__ void resetTimer();
+     static __inline__ struct timeval timevalDiff(struct timeval a, struct timeval b);
+     static __inline__ struct timeval timevalSum(struct timeval a, struct timeval b);
+     static __inline__ void sleep_ms(int milliseconds);
+     static __inline__ struct timeval elapsed();
+     static __inline__ struct timeval getSplit();
+     static __inline__ float hm_add(float a, float b);
+/* FUNCTION DECLARATIONS end. */
+
+/*** FUNCTION DEFINITIONS */
+
+#ifndef BAOTIME_LONGLONG_ENABLED
+     static __inline__ void startTimer(){
+          clock_gettime(CLOCK_REALTIME, &baotime_start);
+     }
+     static __inline__ double getTimerNs(){
+          clock_gettime(CLOCK_REALTIME, &baotime_end);
+          return ((baotime_end.tv_sec - baotime_start.tv_sec) * 1000000000) +
+                         (baotime_end.tv_nsec - baotime_start.tv_nsec); /* in nanoseconds */
+     }
+     static __inline__ double getTimerMs(){
+          clock_gettime(CLOCK_REALTIME, &baotime_end);
+          return
+               ((baotime_end.tv_sec - baotime_start.tv_sec) * 1000) +
+               ((baotime_end.tv_nsec - baotime_start.tv_nsec) / 1000000); /* in milliseconds */
+     }
+#endif
 
 #ifdef BAOTIME_LONGLONG_ENABLED
-int gettimeofday(struct timeval * tp, struct timezone * tzp __attribute__((unused))) /*  __attribute__((unused)) is a GCC flag to suppress the -Wunused-parameter for this variable */
-{
+int gettimeofday(struct timeval* tp, struct timezone* tzp){
     /* Note: some broken versions only have 8 trailing zero's, the correct epoch has 9 trailing zero's
      * This magic number is the number of 100 nanosecond intervals since January 1, 1601 (UTC)
      * until 00:00:00 January 1, 1970 */
@@ -73,37 +144,7 @@ int gettimeofday(struct timeval * tp, struct timezone * tzp __attribute__((unuse
 #endif /* #ifdef WIN32 */
 /**********************************/
 
-/* globals: */
-struct timeval __BAOTIME_H__stop, __BAOTIME_H__startTime, __BAOTIME_H__stopTime, __BAOTIME_H__difference, __BAOTIME_H__savedTime, __BAOTIME_H__updatedTime, __BAOTIME_H__splitTime;
-bool __BAOTIME_H__running = false;
-/**********************************/
 
-
-
-
-/* prototypes: */
-static __inline__ float sec_to_hm(unsigned long int sec);
-static __inline__ unsigned long int hm_to_sec(float hm);
-
-#ifdef BAOTIME_LONGLONG_ENABLED
-     static __inline__ void pauseTimer();
-     static __inline__ void startTimer();
-     static __inline__ void splitTimer();
-     static __inline__ void getTime();
-     static __inline__ void stopTimer();
-     static __inline__ void timertoggle();
-#endif /* #ifdef BAOTIME_LONGLONG_ENABLED */
-
-static __inline__ void resetTimer();
-static __inline__ struct timeval timevalDiff(struct timeval a, struct timeval b);
-static __inline__ struct timeval timevalSum(struct timeval a, struct timeval b);
-static __inline__ void sleep_ms(int milliseconds);
-static __inline__ struct timeval elapsed();
-static __inline__ struct timeval getSplit();
-static __inline__ float hm_add(float a, float b);
-
-
-/**********************************/
 
 static __inline__ float hm_add(float a, float b){
      return sec_to_hm(hm_to_sec(a)+hm_to_sec(b));}
@@ -222,6 +263,9 @@ static __inline__ uint16_t getDay(){
      sprintf(out, "%d", getYear());*/
      /*out[7]='\0';*/
      /*return out;}*/
-     
+/* FUNCTION DEFINITIONS end. */
 
+#ifdef __cplusplus
+     }
+#endif
 #endif /* _BAOTIME_H_ */
